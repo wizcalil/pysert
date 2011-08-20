59d0c0c06d241720bb1f947095edfca64be249de
gnomemory@gmail.com <gnomemory@gmail.com@7609a44f-8b54-6463-f38f-51f69f2d3307>
2011-08-20 21:51:43 +0000

    Added new data set classes . Added command arguments parser .

--- src/pysert.py
+++ src/pysert.py
@@ -1,12 +1,27 @@
 '''
-Created on Aug 18, 2011
+@author: nomemory (Andrei Ciobanu)
+@version: 0.1
+'''
 
-Contains all the data sets available in pyServ .
+'''
+@license: 
+Copyright 2011 Andrei N. Ciobanu
 
-@author: nomemory
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 '''
 
 import abc
+import argparse
 import random
 import re
 import string
@@ -37,7 +52,7 @@ class AbstractDataSet(object):
     def next_value(self):
         return
 #------------------------------------------------------------------------------
-class RandomNumber(AbstractDataSet):
+class Number(AbstractDataSet):
     '''
     ds_dict will contain the following:
             { 
@@ -53,9 +68,9 @@ class RandomNumber(AbstractDataSet):
         func = random.uniform if self.floating == 'True' else random.randint
         return func(int(self.min), int(self.max))
 #------------------------------------------------------------------------------
-class RandomText(AbstractDataSet):
+class Text(AbstractDataSet):
     '''
-    ds_dict will contain the following .
+    ds_dict will contain the following :
     {
         "length" : "<integer value>"
         "uppercase" : "<boolean value>"
@@ -70,11 +85,9 @@ class RandomText(AbstractDataSet):
         super().__init__(ds_dict)
         self.letters = ' '
         if self.uppercase == 'True':
-            self.letters = self.letters.replace(' ', '')
             self.letters = self.letters + string.ascii_uppercase
         if self.lowercase == 'True':
             self.letters = self.letters + string.ascii_lowercase
-            self.letters = self.letters.replace(' ', '')
         
     def next_value(self):
         '''
@@ -85,6 +98,55 @@ class RandomText(AbstractDataSet):
         for i in range(iter):
             rd = random.randint(0, 1000) % len(self.letters)
             ret = ret + self.letters[rd] 
+        return ret
+#------------------------------------------------------------------------------
+class Name(AbstractDataSet):
+    '''
+    ds_dict will contain the following :
+        {
+            "firstname" : "<boolean value>",
+            "lastname" : "<boolean value>"
+        }
+    '''
+    
+    ''' Popular first names in 2010 '''
+    fname = ['Ava', 'Aaron', 'Agathe', 'Agnes', 'Alba', 'Alexander', 'Alexis', \
+    'Alvaro', 'Andrew', 'Andrei', 'Angelina', 'Anthony', 'Anna', 'Ariana', \
+    'Brian', 'Bogdan', 'Carmen', 'Cristopher', 'Connor', 'Daan', \
+    'Daniel', 'David', 'Diego', 'Ella', 'Elizabeth', 'Elsa', \
+    'Emma', 'Enzo', 'Ethan', 'Gabriel', 'Grace', 'Gustav', \
+    'Isaac', 'Jacob', 'Javier', 'Jayden', 'John', 'Juliette', \
+    'Kacper', 'Lars', 'Leah', 'Levi', 'Logan', 'Lotte', 'Lucas', \
+    'Lieke', 'Linus', 'Lucia', 'Mateusz', 'Maxime', 'Melvin', \
+    'Mia', 'Michael', 'Mikolaj', 'Milan', 'Natalie', 'Natalia', \
+    'Olivia', 'Oscar', 'Pablo', 'Paul', 'Paula', 'Piotr', 'Quentin', \
+    'Sarah', 'Samuel', 'Sophia', 'Sem', 'Szymon', 'Thijs', 'Teodore', \
+    'Valeria', 'Valter', 'William', 'Wilma' ]
+    
+    ''' Last names '''
+    lname = ['Abbott', 'Alcott', 'Antonescu', 'Bartok', 'Bayard', 'Banciu', \
+    'Bethmann', 'Bergen', 'Botev', 'Brown', 'Bush', 'Corvinus', \
+    'Chehachkov', 'Dimitrof', 'Dinev', 'Delano', 'Eisenhower',  'Enescu', \
+    'Frels', 'Fugger', 'Gilman', 'Hancock', 'Hoza', 'Ionescu', 'Iordache', \
+    'Kalish', 'Kafka', 'Krasniki', 'Lukasewicz', 'McCormick', 'Medici', \
+    'Menier', 'Morgan', 'Palagyi', 'Parrocel', 'Romanov', 'Rozycki', \
+    'Rufus', 'Olaru', 'Otis', 'Schoenberger', 'Strauss', 'Somoza', \
+    'Sowinski', 'Szigete', 'Tessedik', 'Tisch', 'Vajda', 'Vlas', \
+    'Walker', 'Warhola', 'Varchol', 'Wojnar', 'Zelenjcik']
+            
+    def next_value(self):
+        '''
+        Returns a random string based on ds_dict properties 
+        '''
+        ret = ''
+        if self.firstname == 'True':
+            fname_idx = random.randint(0, 1000) % len(self.fname) 
+            ret = ret + self.fname[fname_idx]
+        if self.lastname == 'True':
+            lname_idx = random.randint(0, 1000) % len(self.lname)
+            if len(ret) > 0:
+                ret = ' ' + ret
+            ret = ret + self.lname[lname_idx]
         return ret
 #------------------------------------------------------------------------------
 class Sequence(AbstractDataSet):
@@ -145,11 +207,12 @@ class DataSetEvaluator(object):
         self.__elem_tree.parse(xml_filename)
         
         #Initialize class attributes
-        self.instances = self.__get_instances()
-        self.iterations = int(self.__get_iterations())
-        self.template = self.__get_template()
+        self.instances = self.init_instances()
+        self.instances_values = self.update_iterations_values()
+        self.iterations = int(self.init_iterations())
+        self.template = self.init_template()
     
-    def __get_instances(self):
+    def init_instances(self):
         '''
         Parse __elem_tree to determine and instantiate the data
         set objects present in the XML file . 
@@ -168,43 +231,63 @@ class DataSetEvaluator(object):
             instances[dataset_name] = dsb.instantiate(dataset_type, ds_dict)
         return instances
     
-    def __get_iterations(self):
+    def init_iterations(self):
         '''
         Returns the number of iterations (how many subsequent lines
         to generate)
         '''
         return int(self.__elem_tree.getroot().attrib['iterations'])
     
-    def __get_template(self):
+    def init_template(self):
         '''
         Retrieves the template string from the XML file 
         '''
         return self.__elem_tree.findall('template')[0].text
+    
+    def update_iterations_values(self):
+        '''
+        Keep a dictionary with instances values
+        '''
+        return {key : instance.next_value() for (key, instance) in \
+                self.instances.items()}
     
     def write_output(self, output=sys.stdout):
         '''
         Parse the template and write the output to a stream .
         The default stream is sys.stdout . 
         '''
+        # Find all #{data_set_names} and replace them with
+        # self.instances[data_set_name].next_value()
+        regex = '(?:^|(?<=[^#]))#{\w+}'
         def inner_subst(matchobj):
-            '''
-            Function needed for re.sub()
-            '''
             #Removing unneeded characters from key
             key = matchobj.group(0)
             for c in ['{','#','}']:
                 key = key.replace(c,'')
-            return str(self.instances[key].next_value())
-        # Find all #{data_set_names} and replace them with
-        # self.instances[data_set_name].next_value()
-        regex = '(?:^|(?<=[^#]))#{\w+}'
+            #replace #{word} with instance values .
+            return str(self.instances_values[key])
         for i in range(self.iterations):
-             output.write(re.sub(regex, inner_subst, self.template) + '\n')
+            output.write(re.sub(regex, inner_subst, self.template) + '\n')
+            self.instances_values = self.update_iterations_values()
+        
 #------------------------------------------------------------------------------
 
 if __name__ == '__main__':
-    dsv = DataSetEvaluator('tmpl.xml')  
-    print(dsv.template)
-    dsv.write_output()
-    rs = RandomText({'length' : '10', 'uppercase':'False', 'lowercase':'True'})
-    print(rs.next_value())
+    ap = argparse.ArgumentParser(description='Generate test data.')
+    ap.add_argument('-i','--input', dest='ifile', help='the input file (XML)')
+    ap.add_argument('-o','--output', dest='ofile',
+                     help='the output file (TEXT)')
+    
+    results = ap.parse_args()
+    if results.ifile is None:
+        ap.error('Input file (IFILE) cannot be empty')
+        sys.exit(-1)
+    
+    dsv = DataSetEvaluator(results.ifile)
+    
+    if results.ofile is None:
+        dsv.write_output()
+    else:
+        out = open(results.ofile, mode='w')
+        dsv.write_output(output=out)
+        out.close()
